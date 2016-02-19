5cf0a6e60b88d771b6727f00ed2f008aaa0c253c
jsmith <jsmith@Dev3-E6530.MS.local>
2016-02-19 09:42:36 -0800

    microsoft handshake

--- htm/api/token.php
+++ htm/api/token.php
@@ -1,177 +1,65 @@
 <?php
-class AccessTokenAuthentication {
-    /*
-     * Get the access token.
-     *
-     * @param string $grantType    Grant type.
-     * @param string $scopeUrl     Application Scope URL.
-     * @param string $clientID     Application client ID.
-     * @param string $clientSecret Application client ID.
-     * @param string $authUrl      Oauth Url.
-     *
-     * @return string.
-     */
-                                    function getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl){
-                                    try {
-            //Initialize the Curl Session.
-            $ch = curl_init();
-            //Create the request Array.
-            $paramArr = array (
-                                    'grant_type'    => $grantType,
-                 'scope'         => $scopeUrl,
-                 'client_id'     => $clientID,
-                 'client_secret' => $clientSecret
-            );
-            //Create an Http Query.//
-            $paramArr = http_build_query($paramArr);
-            //Set the Curl URL.
-            curl_setopt($ch, CURLOPT_URL, $authUrl);
-            //Set HTTP POST Request.
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            //Set data to POST in HTTP "POST" Operation.
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $paramArr);
-            //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
-            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            //Execute the  cURL session.
-            $strResponse = curl_exec($ch);
-            //Get the Error Code returned by Curl.
-            $curlErrno = curl_errno($ch);
-            if($curlErrno){
-                $curlError = curl_error($ch);
-                throw new Exception($curlError);
-            }
-            //Close the Curl Session.
-            curl_close($ch);
-            //Decode the returned JSON string.
-            $objResponse = json_decode($strResponse);
-            if ($objResponse->error){
-                throw new Exception($objResponse->error_description);
-            }
-            return $objResponse->access_token;
-        } catch (Exception $e) {
-            echo "Exception-".$e->getMessage();
-        }
-    }
-}
+
+$domain = 'managedsolutioncsptesting.onmicrosoft.com';
+$authString = "https://login.microsoftonline.com/" . $domain;
+$clientId = 'c9d95c0e-8d97-4bba-b3a1-05bad83f7300';
+$key = 'rW2Mvtwa2Pkc0Bt2iI43eHDNoZmDwOsvhUE5hapsGlM=';
+$clientCred = $clientId . ' ' . $key;
+$resource = 'https://graph.windows.net';
+$token;
+
 /*
- * Class:HTTPTranslator
- * 
- * Processing the translator request.
- */
-Class HTTPTranslator {
-    /*
-     * Create and execute the HTTP CURL request.
-     *
-     * @param string $url        HTTP Url.
-     * @param string $authHeader Authorization Header string.
-     * @param string $postData   Data to post.
-     *
-     * @return string.
-     *
-     */
-    function curlRequest($url, $authHeader, $postData=''){
-        //Initialize the Curl Session.
-        $ch = curl_init();
-        //Set the Curl url.
-        curl_setopt ($ch, CURLOPT_URL, $url);
-        //Set the HTTP HEADER Fields.
-        curl_setopt ($ch, CURLOPT_HTTPHEADER, array($authHeader,"Content-Type: text/xml"));
-        //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, False);
-        if($postData) {
-            //Set HTTP POST Request.
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            //Set data to POST in HTTP "POST" Operation.
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-        }
-        //Execute the  cURL session.
-        $curlResponse = curl_exec($ch);
-        //Get the Error Code returned by Curl.
-        $curlErrno = curl_errno($ch);
-        if ($curlErrno) {
-            $curlError = curl_error($ch);
-            throw new Exception($curlError);
-        }
-        //Close a cURL session.
-        curl_close($ch);
-        return $curlResponse;
-    }
-    /*
-     * Create Request XML Format.
-     *
-     * @param string $languageCode  Language code
-     *
-     * @return string.
-     */
-    function createReqXML($languageCode) {
-        //Create the Request XML.
-        $requestXml = '<ArrayOfstring xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">';
-        if($languageCode) {
-            $requestXml .= "<string>$languageCode</string>";
-        } else {
-            throw new Exception('Language Code is empty.');
-        }
-        $requestXml .= '</ArrayOfstring>';
-        return $requestXml;
-    }
-}
-try {
-    //Client ID of the application.
-    $clientID       = "c9d95c0e-8d97-4bba-b3a1-05bad83f7300";
-    //Client Secret key of the application.
-    $clientSecret = "rW2Mvtwa2Pkc0Bt2iI43eHDNoZmDwOsvhUE5hapsGlM=";
-    //OAuth Url.
-    $authUrl      = "https://login.windows.net/<domain>.onmicrosoft.com/oauth2/token?api-version=1.0";
-    //Application Scope Url
-    $scopeUrl     = "http://api.microsofttranslator.com";
-    //Application grant type
-    $grantType    = "client_credentials";
-    //Create the AccessTokenAuthentication object.
-    $authObj      = new AccessTokenAuthentication();
-    //Get the Access token.
-    $accessToken  = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
-    //Create the authorization Header string.
-    $authHeader = "Authorization: Bearer ". $accessToken;
-    
-    //Create the Translator Object.
-    $translatorObj = new HTTPTranslator();
-    
-    //Input String.
-    $inputStr = 'This is the sample string.';
-    //HTTP Detect Method URL.
-    $detectMethodUrl = "http://api.microsofttranslator.com/V2/Http.svc/Detect?text=".urlencode($inputStr);
-    //Call the curlRequest.
-    $strResponse = $translatorObj->curlRequest($detectMethodUrl, $authHeader);
-    //Interprets a string of XML into an object.
-    $xmlObj = simplexml_load_string($strResponse);
-    foreach((array)$xmlObj[0] as $val){
-        $languageCode = $val;
-    }
-    /*
-     * Get the language Names from languageCodes.
-     */
-    $locale = 'en';
-    $getLanguageNamesurl = "http://api.microsofttranslator.com/V2/Http.svc/GetLanguageNames?locale=$locale";
-    //Create the Request XML format.
-    $requestXml = $translatorObj->createReqXML($languageCode);
-    //Call the curlRequest.
-    $curlResponse = $translatorObj->curlRequest($getLanguageNamesurl, $authHeader, $requestXml);
-    //Interprets a string of XML into an object.
-    $xmlObj = simplexml_load_string($curlResponse);
-    echo "<table border=2px>";
-    echo "<tr>";
-    echo "<td><b>LanguageCodes</b></td><td><b>Language Names</b></td>";
-    echo "</tr>";
-    foreach($xmlObj->string as $language){
-        echo "<tr><td>".$inputStr."</td><td>". $languageCode."(".$language.")"."</td></tr>";
-    }
-    echo "</table>";
-} catch (Exception $e) {
-    echo "Exception: " . $e->getMessage() . PHP_EOL;
+$stCurl = curl_init();
+curl_setopt($stCurl, CURLOPT_URL, 'https://login.windows.net/managedsolutioncsptesting.onmicrosoft.com/oauth2/token?api-version=1.0');
+curl_setopt($stCurl, CURLOPT_PORT, 443);
+curl_setopt($stCurl, CURLOPT_VERBOSE, 0);
+curl_setopt($stCurl, CURLOPT_HEADER, 0);
+curl_setopt($stCurl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($stCurl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($stCurl, CURLOPT_RETURNTRANSFER, 1);
+
+$checkCurl = curl_exec($stCurl);
+if (!curl_errno($stCurl)) {
+    $info = curl_getinfo($stCurl);
+    echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+} else {
+    echo 'Curl error: ' . curl_error($stCurl);
 }
 
-    
+curl_close($stCurl);
+print_r($stCurl);
+ */
+ 
+
+$data = array(
+    'grant_type' => "client_credentials",    
+    'resource' => $resource,
+    'client_id' => $clientId,
+    'client_secret' => $key
+);
+$content = json_encode($data);
+$tuCurl = curl_init();
+curl_setopt($tuCurl, CURLOPT_URL, 'https://login.windows.net/managedsolutioncsptesting.onmicrosoft.com/oauth2/token?api-version=1.0');
+curl_setopt($tuCurl, CURLOPT_PORT, 443);
+curl_setopt($tuCurl, CURLOPT_VERBOSE, 0);
+curl_setopt($tuCurl, CURLOPT_HEADER, 0);
+curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($tuCurl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($tuCurl, CURLOPT_POST, 1);
+curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-length: " . sizeof($content)));
+
+$token = curl_exec($tuCurl);
+if (!curl_errno($tuCurl)) {
+    $info = curl_getinfo($tuCurl);
+    echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+} else {
+    echo 'Curl error: ' . curl_error($tuCurl);
+}
+
+curl_close($tuCurl);
+var_dump($token) . '<br>';
+var_dump($data) . '<br>';
+var_dump($content) . '<br>';
+?> 
