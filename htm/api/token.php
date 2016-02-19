b323cd953e15c29ac3cb19991fae682dc874da40
jsmith <jsmith@Dev3-E6530.MS.local>
2016-02-19 12:02:36 -0800

    microsoft handshake

--- htm/api/token.php
+++ htm/api/token.php
@@ -12,7 +12,7 @@ $grantType = "client_credentials";
 //Create the AccessTokenAuthentication object.
 $authObj = new AccessTokenAuthentication();
 //Get the Access token.
-$accessToken = $authObj->getTokens($grantType, $resource, $clientID, $clientSecret, $authUrl);
+$accessToken = $authObj->getTokens($authUrl, $grantType, $resource, $clientID, $clientSecret);
 //Create the authorization Header string.
 $authHeader = "Authorization: Bearer " . $accessToken;
 
