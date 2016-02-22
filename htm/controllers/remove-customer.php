a2f776a52a6198e5376c674c3185880a6b2e172d
jsmith <jsmith@Dev3-E6530.MS.local>
2016-02-22 11:35:52 -0800

    Fixed DB issue ext .phtml

--- htm/controllers/remove-customer.php
+++ htm/controllers/remove-customer.php
@@ -15,7 +15,7 @@ $sql = "UPDATE customer set active='0' WHERE id='".$custID."'";
 $result = $conn->query($sql);
 
 if ($conn->query($sql) === TRUE) {
-             header('Location: ../portal/admin.htm');
+             header('Location: ../portal/admin.phtml');
         } else {
             echo "Error updating record: " . $conn->error;
         }
