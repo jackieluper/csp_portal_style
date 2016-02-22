0e2b4260bb0215f77c4826c5dcc6f73328b4d2a4
jsmith <jsmith@Dev3-E6530.MS.local>
2016-02-22 10:54:31 -0800

    Working handshake into validation

--- htm/controllers/validateUser.php
+++ htm/controllers/validateUser.php
@@ -17,7 +17,7 @@ if (isset($_POST['userID'])) {
     echo 'Session: ' . $_SESSION['username'];
     echo '<br>';
     echo 'POST: ' . $_POST['userID'];
-    header("Location: ../controllers/config.php");
+    header("Location: ../portal/products.php");
 }
 
 $clientID = "1aa67243-0a91-49ca-9689-9880f15172cf";
