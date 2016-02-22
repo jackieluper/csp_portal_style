007caf41d49c7b5bab303fbdecc4f543b3080d7b
jsmith <jsmith@Dev3-E6530.MS.local>
2016-02-22 10:46:15 -0800

    Working handshake into validation

--- htm/controllers/config.php
+++ htm/controllers/config.php
@@ -4,7 +4,7 @@ Date: 2/17/16
 Managed Solution
 -->
 <?php
-$servername = "104.40.59.186";
+$servername = "localhost";
 $username = "root";
 $password = "Ltd6530*%";
 $dbname = "discount_estimates";
@@ -15,6 +15,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+else{
+    echo 'success!';
 }
 ?>
 
