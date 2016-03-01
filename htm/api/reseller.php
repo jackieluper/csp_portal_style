6285bd3390d7e4d1192c2413f55dd2e932645920
jsmith <jsmith@Dev3-E6530.MS.local>
2016-03-01 10:20:15 -0800

    Added tokens and comments/merging new cust with microsoft api

--- htm/api/reseller.php
+++ htm/api/reseller.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 include 'sa-token.php';
 $token = $_SESSION['sa_token'];
@@ -17,20 +18,22 @@ function getGUID() {
     return $uuid;
 }
 
+try {
 
-    $guid = getGUID();
-    $guidCor = getGUID();
+    $guid = (string) getGUID();
+    $guidCor = (string) getGUID();
     $header = array();
-    $header[] = "api-version: 2015-03-31";
-    $header[] = "Authorization: Bearer $token";
-    $header[] = "Accept: application/json";    
-    $header[] = "x-ms-correlation-id: $guidCor";
-    $header[] = "x-ms-tracking-id: $guid";
-   
+    $header[] = "api-version : 2015-03-31";
+    $header[] = "Authorization : Bearer $token";
+    $header[] = "Content-Type: application/x-www-form-urlencoded";
+    $header[] = "Accept: application/json";
+    $header[] = "x-ms-correlation-id $guidCor";
+    $header[] = "x-ms-tracking-id : $guid";
+
 
     //Initialize the Curl Session.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://api.cp.microsoft.com/customers/get-by-identity?provider=AAD&type=tenant&tid=22e38d40-62cb-47c4-afdf-19421c5522c0");
+    curl_setopt($ch, CURLOPT_URL, 'https://api.cp.microsoft.com/customers/get-by-identity?provider=AAD&type=tenant&tid=22e38d40-62cb-47c4-afdf-19421c5522c0');
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, $header);
@@ -38,4 +41,7 @@ function getGUID() {
     echo curl_error($ch) . '<br>';
     curl_close($ch);
     $objResponse = json_decode($strResponse);
-    echo 'ID: ' . print_r($strResponse);
+    echo 'ID: ' . $objResponse->id;
+} catch (Exception $e) {
+    echo "Exception-" . $e->getMessage();
+}
