4fbc72f4345dfadaeecb152667393803f01ea64c
Christian Boynton <cboynton@cweil.com>
2016-03-21 22:52:08 -0700

    comment out old login code

--- htm/portal/login_page.php
+++ htm/portal/login_page.php
@@ -99,16 +99,16 @@ if (!empty($_GET['code'])) {
             <span class="sr-only">Next</span>
         </a>
     </div>
-    <form class="form-inline" id="loginForm" action="../controllers/validateUser.php" method="post">
-        <div class="form-group" >
-            <label class="lbl">UserName</label>
-            <input class="form-control" type="text" name="userId" id="userId" placeholder="required" required><br>
-            <label class="lbl">Password</label>
-            <input class="form-control" type="password" name="password" placeholder="required" required><br>             
-            <input class="loginBtn" type="Submit" name="loginBtn" value="Sign-in" ><br>
-            <a class="link" id="regLink" href="registration.php">Have not registered yet? Click Here!</a>
-        </div>
-    </form>  
+<!--    <form class="form-inline" id="loginForm" action="../controllers/validateUser.php" method="post">-->
+<!--        <div class="form-group" >-->
+<!--            <label class="lbl">UserName</label>-->
+<!--            <input class="form-control" type="text" name="userId" id="userId" placeholder="required" required><br>-->
+<!--            <label class="lbl">Password</label>-->
+<!--            <input class="form-control" type="password" name="password" placeholder="required" required><br>             -->
+<!--            <input class="loginBtn" type="Submit" name="loginBtn" value="Sign-in" ><br>-->
+<!--            <a class="link" id="regLink" href="registration.php">Have not registered yet? Click Here!</a>-->
+<!--        </div>-->
+<!--    </form>  -->
     <div class="login">
 
         <div class="login-text">Login with your Microsoft Work Account!<br>
