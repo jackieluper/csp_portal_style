65535c45c86c8ddcbf31c9c47a632ef155f1964a
jsmith <jsmith@Dev3-E6530.MS.local>
2016-03-31 15:03:50 -0700

    Add customer works/ testing logins

--- htm/api/client/authorize_ad.php
+++ htm/api/client/authorize_ad.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once '_init.php';
-require '../../controllers/config.php';
+require 'billing_portal_php/htm/controllers/config.php';
 $authCode = $_GET['code'];
 $userAuth = new UserAuth();
 $userAuth->requestAdTokenForAuthCode($authCode);
