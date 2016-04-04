36f6e37cc0d53de7671b55a74eb5ec332a50375f
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 11:25:04 -0700

    Testing new customer/new customer login

--- htm/controllers/validateUser.php
+++ htm/controllers/validateUser.php
@@ -23,13 +23,12 @@ $sqlCompanyCheck = "SELECT id from customer where customer_name='$company_name'"
 $resCompanyCheck = $conn->query($sqlCompanyCheck);
 if ($resCompanyCheck->num_rows > 0) {
     $customer_id = $row['id'];
-} 
+}
 $sqlCompanyCheck = "SELECT username from user where username='$user_name'";
 $resCompanyCheck = $conn->query($sqlCompanyCheck);
 if ($resCompanyCheck->num_rows > 0) {
     $user_name = $row['username'];
-} 
-else {
+} else {
     $sqlAddCompany = "INSERT INTO customer(customer_name, entity_type, company_tid, is_provised, primary_domain, relationship, discount, active)
             VALUES('$company_name', 'Corporate', '$tid', '0', '$company_domain', 'Cloud Reseller', '0', '1')";
     if ($conn->query($sqlAddCompany) == True) {
@@ -55,11 +54,11 @@ else {
 $sqlRole = "select role from user where username='$user_name'";
 $resRole = $conn->query($sqlRole);
 if ($resRole->num_rows > 0) {
-    $role = $row['role'];
-    $user->setRole($role);
-    
-}
-else{
+    while ($row = $resRole->fetch_assoc()) {
+        $role = $row['role'];
+        $user->setRole($role);
+    }
+} else {
     echo "Error: " . $sqlRole . "<br>" . $conn->error;
 }
 $resEntity = $conn->query("select entity_type from customer where customer_name='$company_name'");
