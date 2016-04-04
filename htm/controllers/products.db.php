23a23f4b4747e7f9975ebf9276bba4b1008d1754
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 12:39:08 -0700

    Testing Billing

--- htm/controllers/products.db.php
+++ htm/controllers/products.db.php
@@ -24,10 +24,10 @@ $offers = new offers();
 
 for ($i = 0; $i < count($hotOffers); $i++) {
 //Grabbing the product information to display in top offers
-    $resultOffer1 = $conn->query("select offer.display_name, list_price from offer, offer_price where offer.id='$hotOffers[$i]' and offer_id='$hotOffers[$i]'");
+    $resultOffer1 = $conn->query("select offer.display_name, erp_price from offer, offer_price where offer.id='$hotOffers[$i]' and offer_id='$hotOffers[$i]'");
     while ($row = $resultOffer1->fetch_assoc()) {
         $name = $row['display_name'];
-        $price = $row['list_price'];
+        $price = $row['erp_price'];
         $id = $row['id'];
     }
     $topOffers->setOffer($index, $offer);
@@ -49,11 +49,11 @@ for ($i = 0; $i < count($hotOffers); $i++) {
     $index++;
 }
 
-$result1 = $conn->query("select offer.id, offer.display_name, offer.license_agreement_type, offer.purchase_unit, offer.sku, offer_price.list_price from offer, offer_price where offer.id=offer_price.id and offer.license_agreement_type='$entity'");
+$result1 = $conn->query("select offer.id, offer.display_name, offer.license_agreement_type, offer.purchase_unit, offer.sku, offer_price.erp_price from offer, offer_price where offer.id=offer_price.id and offer.license_agreement_type='$entity'");
 $index = 0;
 while ($row = $result1->fetch_assoc()) {
     $name = $row['display_name'];
-    $erp = $row['list_price'];
+    $erp = $row['erp_price'];
     $purchase_unit = $row['purchase_unit'];
     $id = $row['id'];
     $offers->setOfferName($index, $name);
