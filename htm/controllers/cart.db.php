f26850dd3d002c52cf39d241bb8500625922b7e9
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 00:22:05 -0700

    Testing create new order

--- htm/controllers/cart.db.php
+++ htm/controllers/cart.db.php
@@ -18,23 +18,22 @@ while ($row = $result->fetch_assoc()) {
     $name = $row['item_name'];
     $msrp = $row['msrp'];
     $qty = $row['qty'];
-    $total1 = $msrp * $qty;
-    $total1 += $total + $total1;
+    $total += $msrp * $qty;
     $cart->setItem($index, $item);
     $cart->setName($index, $name);
     $cart->setMsrp($index, $msrp);
     $cart->setQty($index, $qty);
     $index++;
 }
-$cart->setTotal($total1);
+$cart->setTotal($total);
 //Calculate customer discount if any and display savings as well as new total
 $sqlDiscount = "Select discount from customer where customer_name='" . $_SESSION['company_name'] . "'";
 $resultDiscount = $conn->query($sqlDiscount);
 while ($row = $resultDiscount->fetch_assoc()) {
     $rate = $row['discount'];
     $discountDec = $rate / 100;
-    $discountNum = $total1 * $discountDec;
-    $total = $total1 - $discountNum;
+    $discountNum = $total * $discountDec;
+    $total = $total - $discountNum;
     $cart->setDiscount($discountNum);
     $cart->setDiscountRate($rate);
 }
