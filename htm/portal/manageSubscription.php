d49bfa2559ac10121129b3bcbeb13d7027e03e4f
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 03:55:14 -0700

    Create new orders works!/Implementing manage subscriptions

--- htm/portal/manageSubscription.php
+++ htm/portal/manageSubscription.php
@@ -61,7 +61,7 @@ $subscriptionList = $subscription->getSubscriptionList();
                 ?>
                 <tr>
                     <td><?php echo $subscriptionList[$i]->getFriendlyName() ?></td>
-                    <td><input type="number" step="1"><?php echo $subscriptionList[$i]->getQuantity() ?></input></td>
+                    <td><input type="number" step="1" value="<?php echo $subscriptionList[$i]->getQuantity() ?>"></input></td>
                 </tr>
             <?php } 
 
