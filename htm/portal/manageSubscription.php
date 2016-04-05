ccb60d983c92f7f92370ec98c4177f816dced7a2
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 03:41:40 -0700

    Create new orders works!/Implementing manage subscriptions

--- htm/portal/manageSubscription.php
+++ htm/portal/manageSubscription.php
@@ -51,7 +51,7 @@ for($i = 0; $i < count($subscriptionList); $i++){
     echo var_dump($subscriptionList[$i]->getFriendlyName()) . '<br>';
 }
 
-//$subscriptionList[0]->updateFriendlyName();
+$subscriptionList[0]->updateFriendlyName("Office 365 Enterprise E3 ");
 //$subscriptionList[0]->updateQuantity(1);
 //$subscriptionList[0]->getAddOnList();
 
