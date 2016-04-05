f8667ab4d92ab204b14997863e863277619a97c8
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 23:59:04 -0700

    Testing create new order

--- htm/portal/billing.php
+++ htm/portal/billing.php
@@ -326,7 +326,7 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
             $discountRate = $product->{'discount-rate'};
             $totalSavings = $product->{'discount-amount'};
             $order = new Order($tid);
-            $order->addOrderItem($sku, $name, $qty);  
+            $order->addOrderItem("$sku", "$name", $qty);  
             $order->submitOrder();
             $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES(" . $_SESSION['custId'] . ", '$itemNum', '$sku', '$name', '1 month(s)', '$cost', '$qtyFormatted', '$discountRate', '$totalSavings', '$amount', $tranId)";
