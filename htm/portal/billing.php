44f42afe0b190f166bba801d375066059cd838c8
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 23:53:57 -0700

    Testing create new order

--- htm/portal/billing.php
+++ htm/portal/billing.php
@@ -301,7 +301,7 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
 
     if ((string) $gwResponse->result == 1) {
         //need to parse customer TID from login
-        $order = new Order($tid);
+        
         print '<div id="print-content">
                 <form>';
         ?>
@@ -325,7 +325,9 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
             $name = $product->description;
             $discountRate = $product->{'discount-rate'};
             $totalSavings = $product->{'discount-amount'};
-            $order->addOrderItem($sku, $name, $qty);            
+            $order = new Order($tid);
+            $order->addOrderItem($sku, $name, $qty);  
+            $order->submitOrder();
             $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES(" . $_SESSION['custId'] . ", '$itemNum', '$sku', '$name', '1 month(s)', '$cost', '$qtyFormatted', '$discountRate', '$totalSavings', '$amount', $tranId)";
             $resultInvoice = $conn->query($sqlInvoice);
@@ -339,8 +341,6 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
         <div><strong>Product Cost: </strong>$' . $costFormatted . '</div>
         <div><strong>Product Quantity: </strong>' . $qtyFormatted . '</div><br>';
         }
-        
-        $order->submitOrder();
         print '
         <div><strong>Discount Rate: ' . $discountRate . '%</strong></div>
         <div><strong>Total Savings: $' . $totalSavings . '</strong></div>
