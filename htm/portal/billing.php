a8e7e429e5390593e2af4ffd11ae11be54c19ff2
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 17:01:36 -0700

    Testing create new order

--- htm/portal/billing.php
+++ htm/portal/billing.php
@@ -301,9 +301,7 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
 
     if ((string) $gwResponse->result == 1) {
         //need to parse customer TID from login
-        $order = new Order($tid);
-        $order->addOrderItem('84A03D81-6B37-4D66-8D4A-FAEA24541538', 'Basic AD for Tech PHP', 1);
-        $order->submitOrder();
+        $order = new Order($_SESSION['tid']);
         
         print '<div id="print-content">
                 <form>';
@@ -328,6 +326,7 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
             $name = $product->description;
             $discountRate = $product->{'discount-rate'};
             $totalSavings = $product->{'discount-amount'};
+            $order->addOrderItem($sku, $name, $qtyFormatted);
             $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES(" . $_SESSION['custId'] . ", '$itemNum', '$sku', '$name', '1 month(s)', '$cost', '$qtyFormatted', '$discountRate', '$totalSavings', '$amount', $tranId)";
             $resultInvoice = $conn->query($sqlInvoice);
@@ -349,6 +348,8 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
         </div>';
         $sqlDelete = "DELETE FROM cart where customer_id='" . $_SESSION['custId'] . "'";
         $resultDelete = $conn->query($sqlDelete);
+        
+        $order->submitOrder();
     }
     ?>
     <input type="button" class="receiptBtn" onclick="printDiv('print-content')" value="Print Receipt"/>
