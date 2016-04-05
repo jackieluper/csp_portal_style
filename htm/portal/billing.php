adac1c4336e5c0223be650c2c54fa91c53b618b5
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-04 17:11:42 -0700

    Testing create new order

--- htm/portal/billing.php
+++ htm/portal/billing.php
@@ -301,9 +301,6 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
 
     if ((string) $gwResponse->result == 1) {
         //need to parse customer TID from login
-        $order = new Order('95e724ab-3834-4d4d-a5f9-6bc725a4d87d');
-        $order->addOrderItem('84A03D81-6B37-4D66-8D4A-FAEA24541538', 'Basic AD for Tech PHP', 1);
-        $order->submitOrder();
         print '<div id="print-content">
                 <form>';
         ?>
