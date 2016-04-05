d9214689d466268d86bc4abceef86f952c5f4032
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 05:05:32 -0700

    Have working list of subscriptions/testing changing qty

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -1,7 +1,9 @@
 <?php
+require_once '../portal/manageSubscription.php';
+
 $qty = $_POST['qty'];
 $i = $_POST['itemNum'];
-$subscriptionList = $subscription->getSubscriptionList();
+
 $subscriptionList[$i]->updateQuantity($qty);
 
-echo "updated qty: " . $subscriptionList[$i]->getQuantity();
+header("Location: ../portal/manageSubscription.php");
