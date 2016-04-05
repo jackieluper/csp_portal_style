23204b9515a24890dccd839d8b731c742e4e0152
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 04:55:58 -0700

    Have working list of subscriptions/testing changing qty

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-require '../controllers/config.php';
-require_once '../api/client/_init.php';
+require_once '../portal/manageSubscription.php';
 
 
 $qty = $_POST['qty'];
 $i = $_POST['itemNum'];
-$subscriptionList[$i]->updateQuantity();
+$subscriptionList[$i]->updateQuantity($qty);
 
 echo "updated qty: " . $subscriptionList[$i]->getQuantity();
