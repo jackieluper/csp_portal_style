7acf547d61b7e57c92dd30d4cf4964d55d4797f6
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 04:51:38 -0700

    Have working list of subscriptions/testing changing qty

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -3,6 +3,9 @@ session_start();
 require '../controllers/config.php';
 require_once '../api/client/_init.php';
 
-echo $_POST['qty'] . '<br>';
-echo $_POST['name'];
 
+$qty = $_POST['qty'];
+$i = $_POST['itemNum'];
+$subscriptionList[$i]->updateQuantity();
+
+echo "updated qty: " . $subscriptionList[$i]->getQuantity();
