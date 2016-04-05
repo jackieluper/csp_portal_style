4e3b9f4e82da66a675a2fdef650d2eb9ee02f479
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 04:36:12 -0700

    Have working list of subscriptions/testing changing qty

--- htm/portal/manageSubscription.php
+++ htm/portal/manageSubscription.php
@@ -62,7 +62,7 @@ require "../api/client/_init.php";
         ?>
     <form action="../controllers/update-qty.php" method="post">
             <tr>
-                <td name="name"><?php echo $subscriptionList[$i]->getFriendlyName() ?></td>
+                <td><input name="name" value="<?php echo $subscriptionList[$i]->getFriendlyName() ?>"</td>
                 <td><input id="qty" name="qty" type="number" step="1" value="<?php echo $subscriptionList[$i]->getQuantity() ?>" style="color: #000"></input></td>
                 <td><button class="checkoutButton" type="submit">Update Quantity</button></td>
             </tr>
