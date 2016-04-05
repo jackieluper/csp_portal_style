227f4cfcc3fa3c29b4dc8cae7f60c474d57eebf5
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-05 09:36:25 -0700

    Have working list of subscriptions/testing changing qty

--- htm/portal/products.php
+++ htm/portal/products.php
@@ -34,18 +34,18 @@ require '../api/client/app/offer.class.php';
     <a href="#" class="nav-toggle-btn">Menu</a>
     <ul>
         <?php if ($_SESSION['role'] >= $userRole) { ?>
-            <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='products.php'>Products</a><br></li>
-            <li><img class='icon' src='../img/icons/software.png' alt='Manage Subscription'><a href="manageSubscription.php">Manage Subscriptions</a><br></li>
-            <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='invoice.php'>Invoice</a><br></li>
-            <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='checkout.php'>Checkout</a><br></li>
+            <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
+            <li><img class='icon' src='../img/icons/software.png' alt='Manage Subscription'><a href="../portal/manageSubscription.php">Manage Subscriptions</a><br></li>
+            <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
+            <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
             <li><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
             <?php if ($_SESSION['role'] == $adminRole) { ?>
-                <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='admin.php'>Administration</a><br></li>
+                <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
                 <?php
             }
         }
         ?>
-        <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='contactUs.php'>Contact us</a><br></li>
+        <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
         <li><img class='icon' src='../img/icons/logout.png' alt='Logout' ><a href='../controllers/logout.php'>Logout</a><br></li>
     </ul>
 </nav>
