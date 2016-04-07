0546a924d7978569d25dc130a2c53932bcbc68e5
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-07 14:03:28 -0700

    Integrating add/remove licenses

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -15,26 +15,25 @@ $subscription_name = $subscriptionList[$i]->getOfferName();
 
 $sqlOfferId = "SELECT id from offer where sku='$subscription_id'";
 $resOfferId = $conn->query($sqlOfferId);
-if($resOfferId->num_rows > 0){
-    while($row = $resOfferId->fetch_assoc()){
+if ($resOfferId->num_rows > 0) {
+    while ($row = $resOfferId->fetch_assoc()) {
         $offerId = $row['id'];
         echo "offer id: " . $offerId . '<br>';
     }
-}
-else{
+} else {
     echo "Error: " . $sqlOfferId . "<br>" . $conn->error;
 }
-    $sqlOfferData = "SELECT list_price, erp_price, from offer_price where offer_id='$offerId'";
-    $resOfferData = $conn->query($sqlOfferData);
-    if($resOfferData->num_rows > 0){
-        while($row = $resOfferData->fetch_assoc()){
-            $list_price = $row['list_price'];
-            $erp_price = $row['erp_price'];
-            echo "list price: " . $list_price . '<br>';
-            echo "erp price: " . $erp_price . '<br>';
-        }
+
+$sqlOfferData = "SELECT list_price, erp_price, from offer_price where offer_id='$offerId'";
+$resOfferData = $conn->query($sqlOfferData);
+if ($resOfferData->num_rows > 0) {
+    while ($row1 = $resOfferData->fetch_assoc()) {
+        $list_price = $row1['list_price'];
+        $erp_price = $row1['erp_price'];
+        echo "list price: " . $list_price . '<br>';
+        echo "erp price: " . $erp_price . '<br>';
     }
-    else {
+} else {
     echo "Error: " . $sqlOfferData . "<br>" . $conn->error;
 }
 
@@ -46,31 +45,30 @@ if ($resProvision->num_rows > 0) {
     }
     $sqlgetTranId = "SELECT transaction_id from transactions order by desc limit 1";
     $resTranId = $conn->query($sqlgetTranId);
-    if($resTranId->num_rows > 0){
+    if ($resTranId->num_rows > 0) {
         $tranId = $row['transaction_id'];
         echo "transaction id: " . $tranId . '<br>';
+    } else {
+        echo "Error: " . $sqlgetTranId . "<br>" . $conn->error;
     }
-    else {
-    echo "Error: " . $sqlgetTranId . "<br>" . $conn->error;
-}
 } else {
     echo "Error: " . $sqlProvision . "<br>" . $conn->error;
 }
- /*
-if ($provision == 1) {
-    $subscriptionList[$i]->updateQuantity($qty);
-    header("Location:../portal/manageSubscription.php");
-} else {
-    $sqlCartCheck = "SELECT * from cart where customer_id='$customer_id'";
-    $resCartCheck = $conn->query($sqlCartCheck);
-    if ($resCartCheck->num_rows > 0) {
-        $sqlDeleteCart = "DELETE from cart where customer_id='$customer_id'";
-        if ($conn->query($sqlAddCompany) == True) {
-            $sqlUpdateQty = "INSERT into cart set(customer_id, items, item_name, our_cost, msrp, proposed_cost, qty, transaction_id)"
-            . "VALUES($customer_id, $subscription_id, $subscrtiption_name, $list_price, $erp_price, $proposed_cost, $qty,   )";
-        }
-    }
-}
+/*
+  if ($provision == 1) {
+  $subscriptionList[$i]->updateQuantity($qty);
+  header("Location:../portal/manageSubscription.php");
+  } else {
+  $sqlCartCheck = "SELECT * from cart where customer_id='$customer_id'";
+  $resCartCheck = $conn->query($sqlCartCheck);
+  if ($resCartCheck->num_rows > 0) {
+  $sqlDeleteCart = "DELETE from cart where customer_id='$customer_id'";
+  if ($conn->query($sqlAddCompany) == True) {
+  $sqlUpdateQty = "INSERT into cart set(customer_id, items, item_name, our_cost, msrp, proposed_cost, qty, transaction_id)"
+  . "VALUES($customer_id, $subscription_id, $subscrtiption_name, $list_price, $erp_price, $proposed_cost, $qty,   )";
+  }
+  }
+  }
 
 
 
