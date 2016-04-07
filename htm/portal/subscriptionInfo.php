fb7b0a527642741bbfe3d1010e1c9e45fda2f161
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-07 09:02:26 -0700

    Added table to new page subscription info/working out look and design of said table.

--- htm/portal/subscriptionInfo.php
+++ htm/portal/subscriptionInfo.php
@@ -57,7 +57,7 @@ $i = $_POST['itemNum'];
         $tag = "noImage.png";
     }
     ?>
-    <image src="../img/microsoft_img/<?php echo $tag ?>" alt="Image not found">
+    <image src="../img/microsoft_img/<?php echo $tag ?>" alt="Image not found" style="float:left;">
     <table class="subscriptionDetails">
         <th class="subscriptionHeader"><?php echo $subscriptionList[$i]->getOfferName() ?></th>
         <tr>
