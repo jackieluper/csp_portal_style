ad89ee4da5d481f616dffb8515d6248f4a049e5f
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-07 12:36:06 -0700

    Integrating add/remove licenses

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -9,7 +9,18 @@ $customer_id = $_SESSION['custId'];
 $customerTenantId = $_SESSION['tid'];
 $subscription = new Subscription($customerTenantId);
 $subscriptionList = $subscription->getSubscriptionList();
-echo $i;
+
+$sqlProvision = "SELECT is_provised from customer where id='$customer_id'";
+$resProvision = $conn->query($sqlProvision);
+if($resProvision->num_rows > 0){
+    while ($row = $resProvision->fetch_assoc()) {
+        $provision = $row['is_provised'];
+    }
+}
+else{
+    echo "Error: " . $sqlProvision . "<br>" . $conn->error;
+}
+echo "provision: " . $provision;
 /*
 $sqlCartCheck = "SELECT * from cart where customer_id='$customer_id'";
 $resCartCheck = $conn->query($sqlCartCheck);
@@ -22,6 +33,6 @@ if($resCartCheck->num_rows > 0){
 }
 */
 
-$subscriptionList[$i]->updateQuantity($qty);
-header("Location:../portal/manageSubscription.php");
+//$subscriptionList[$i]->updateQuantity($qty);
+//header("Location:../portal/manageSubscription.php");
 ?>
