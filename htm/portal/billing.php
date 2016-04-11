bebc2a5d0c0a3b2f6491e28628b1c577a9443e80
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-11 09:45:06 -0700

    added flag to cart for if the order is an update to current subscription.

--- htm/portal/billing.php
+++ htm/portal/billing.php
@@ -328,9 +328,11 @@ if (empty($_POST['DO_STEP_1']) && empty($_GET['token-id'])) {
         $name = $product->description;
         $discountRate = $product->{'discount-rate'};
         $totalSavings = $product->{'discount-amount'};
-            $totalToAdd = $subscriptionList[$i]->getQty() + $qty;
-            $subscriptionList[$i]->update($qty);
-        
+        $subscription = new Subscription($customerTenantId);
+        $subscriptionList = $subscription->getSubscriptionList();
+        $totalToAdd = $subscriptionList[$i]->getQty() + $qty;
+        $subscriptionList[$i]->update($totalToAdd);
+
         $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES(" . $_SESSION['custId'] . ", '$itemNum', '$sku', '$name', '1 month(s)', '$cost', '$qtyFormatted', '$discountRate', '$totalSavings', '$amount', $tranId)";
         $resultInvoice = $conn->query($sqlInvoice);
