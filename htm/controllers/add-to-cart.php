b57cf183b838cb319a4ccd701114b011287963d9
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-11 08:47:41 -0700

    products

--- htm/controllers/add-to-cart.php
+++ htm/controllers/add-to-cart.php
@@ -73,7 +73,8 @@ if ($result3->num_rows > 0) {
         $qty = $row['qty'] + 1;
         $sql4 = "UPDATE cart SET qty='" . $qty . "' where items='" . $offerID . "'";
         if ($conn->query($sql4) === TRUE) {
-            header('Location: ../portal/products.php');
+            //header('Location: ../portal/products.php');
+            echo $erpPrice;
         } else {
             echo "Error updating record: " . $conn->error;
         }
--- htm/controllers/invoice.db.php
+++ htm/controllers/invoice.db.php
@@ -4,14 +4,13 @@ Date: 3/21/16
 Managed Solution
 -->
 <?php
-
 session_start();
 include 'config.php';
 include '../classes/invoice.class.php';
 
 $invoice = new invoice();
 $index = 0;
-$result = $conn->query("SELECT total, transaction_id FROM transactions where customer_id='" . $_SESSION['custId'] . "' GROUP BY transaction_id");
+$result = $conn->query("SELECT total, transaction_id FROM transactions where customer_id='" . $_SESSION['custId'] . "' ORDER BY transaction_id");
 error_reporting(E_ALL ^ E_NOTICE);
 while ($row = $result->fetch_assoc()) {
     $tranTotal = $row['total'];
