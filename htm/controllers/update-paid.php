b5eb31d21769d9ce0c005e2aad1000182219624a
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-11 14:27:48 -0700

    testing logic for updating subscriptions after payment.

--- htm/controllers/update-paid.php
+++ htm/controllers/update-paid.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 require "../controllers/config.php";
 require "../api/client/_init.php";
@@ -14,7 +15,31 @@ $subscription_id = $subscriptionList[$i]->getOfferId();
 $subscription_name = $subscriptionList[$i]->getOfferName();
 
 echo $subscriptionList[$i]->getQuantity();
-$subscriptionList[$i]->updateQuantity($qty); 
+$subscriptionList[$i]->updateQuantity($qty);
+
+$sqlOfferId = "SELECT id from offer where sku='$subscription_id'";
+$resOfferId = $conn->query($sqlOfferId);
+if ($resOfferId->num_rows > 0) {
+    while ($row = $resOfferId->fetch_assoc()) {
+        $offerId = $row['id'];
+        echo "offer id: " . $offerId . '<br>';
+    }
+} else {
+    echo "Error: " . $sqlOfferId . "<br>" . $conn->error;
+}
+
+$sqlOfferData = "SELECT list_price, erp_price from offer_price where offer_id='$offerId'";
+$resOfferData = $conn->query($sqlOfferData);
+if ($resOfferData->num_rows > 0) {
+    while ($row = $resOfferData->fetch_assoc()) {
+        $list_price = $row['list_price'];
+        $erp_price = number_format($row['erp_price'], 2);
+        echo "list price: " . $list_price . '<br>';
+        echo "erp price: " . $erp_price . '<br>';
+    }
+} else {
+    echo "Error: " . $sqlOfferData . "<br>" . $conn->error;
+}
 
 $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES('$customer_id', '1', '$subscription_id', '$subscription_name', '1 month(s)', '$erp_price', '$updateQty', '$discount', '$totalSavings', '$total', $tranId)";
