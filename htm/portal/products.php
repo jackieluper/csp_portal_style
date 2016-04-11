c94d6013bfbc8ed156144c524efe3531766fb736
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-11 16:15:29 -0700

    working details of new products list

--- htm/portal/products.php
+++ htm/portal/products.php
@@ -87,7 +87,7 @@ require '../controllers/cart.db.php';
         <?php
         for ($i = 0; $i < count($topOffers->name); $i++) {
             ?>
-            <table class="topOffers">
+        <table class="topOffers" style="position:center; float:left;">
                 <tr>
                     <td><strong> <?php echo $topOffers->name[$i] ?> </strong></td>
                 </tr>
