1253558b0a11105abfd8eec54c8c62fb3e228f05
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-11 15:10:02 -0700

    testing logic for updating subscriptions after payment.

--- htm/controllers/update-paid.php
+++ htm/controllers/update-paid.php
@@ -66,7 +66,7 @@ $totalSavings = number_format($total * $discount, 2);
 $subscriptionList[$i]->updateQuantity($qty);
 
 $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
-            VALUES('$customer_id', '1', '$subscription_id', '$subscription_name', '1 month(s)', '$erp_price', '$qty', '$updateQty', '$totalSavings', '$total', $tranId)";
+            VALUES('$customer_id', '1', '$subscription_id', '$subscription_name', '1 month(s)', '$erp_price', '$discount', '$updateQty', '$totalSavings', '$total', $tranId)";
 
 if ($conn->query($sqlInvoice) == TRUE) {
     $_SESSION['invoiceId'] = $tranId;
