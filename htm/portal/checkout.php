65d9fb9c1573868e448074400d36923b2e407efc
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-12 07:49:31 -0700

    Fixing qty update on products page for catalog

--- htm/portal/checkout.php
+++ htm/portal/checkout.php
@@ -66,7 +66,7 @@ $role = $_SESSION['role'];
                     <tr>
                         <td><span><?php echo $cart->name[$i] ?></span></td>
                         <td><?php echo $cart->msrp[$i] ?></td>
-                        <td><input step="1" name="qty" value="<?php echo $cart->qty[$i] ?>" style="text-align: right; border-style: none; border-radius: 5px; width: 10%;"></input></td>
+                        <td><input step="1" name="qty" value="<?php echo $cart->qty[$i] ?>" style="color: #000; text-align: right; border-style: none; border-radius: 5px; width: 10%;"></input></td>
                         <td style="align-content: left"><button class="updateQtyBtn" type="submit">Add To Cart</button></td>
                         <td ><a href="../controllers/remove-from-checkout.php?id=<?php echo $cart->item[$i] ?>" ><strong>Remove</strong></a></td>
                     </tr>            
