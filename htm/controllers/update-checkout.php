cc1c0c05fd98002bfcd1741ea962ed6fa2d82673
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-12 08:29:04 -0700

    Working on qty for checkout page

--- htm/controllers/update-checkout.php
+++ htm/controllers/update-checkout.php
@@ -5,13 +5,10 @@ require 'config.php';
 $id = $_POST['id'];
 $qty = $_POST['qty'];
 
-echo 'id: ' . $id . '<br>';
-echo 'qty: '. $qty . '<br>';
 
-/*
 $sql4 = "UPDATE cart SET qty='" . $qty . "' where items='" . $offerID . "'";
         if ($conn->query($sql4) === TRUE) {
-            header('Location: ../portal/products.php');
+            header('Location: ../portal/checkout.php');
         } else {
             echo "Error updating record: " . $conn->error;
         }
