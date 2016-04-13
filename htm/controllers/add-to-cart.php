80c338bf8ec6e38db2b0b368dd6074d866970d73
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-13 11:22:00 -0700

    New images added from Jackie

--- htm/controllers/add-to-cart.php
+++ htm/controllers/add-to-cart.php
@@ -77,7 +77,7 @@ for ($i = 0; $i < count($subscriptionList); $i++) {
     $subscription_id = $subscriptionList[$i]->getOfferId();
     $subscription_name = $subscriptionList[$i]->getOfferName();
     echo $subscription_name . '<br>';
-    if ($subscription_name[$i] == $offerName) {
+    if ($subscription_name == "$offerName") {
         echo 'its in the list.';
     }
 }
