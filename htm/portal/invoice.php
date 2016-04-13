fb1afb4ae7ddcb84f1291d11b8419bcd191d671c
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-13 09:48:26 -0700

    Fixed loading gif to work on click and stop on load. Tested and works

--- htm/portal/invoice.php
+++ htm/portal/invoice.php
@@ -30,43 +30,46 @@ include '../controllers/invoice.db.php';
         <a href="#" class="nav-toggle-btn">Menu</a>
         <ul>
             <?php if ($_SESSION['role'] >= $userRole) { ?>
-            <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
-            <li><img class='icon' src='../img/icons/software.png' alt='Manage Subscription'><a href="../portal/manageSubscription.php">Manage Subscriptions</a><br></li>
-            <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
-            <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
-            <li><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
-            <?php if ($_SESSION['role'] == $adminRole) { ?>
-                <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
-                <?php
+                <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
+                <li><img class='icon' src='../img/icons/software.png' alt='Manage Subscription'><a href="../portal/manageSubscription.php">Manage Subscriptions</a><br></li>
+                <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
+                <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
+                <li><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
+                <?php if ($_SESSION['role'] == $adminRole) { ?>
+                    <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
+                    <?php
+                }
             }
-        }
-        ?>
-        <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
-        <li><img class='icon' src='../img/icons/logout.png' alt='Logout' ><a href='../controllers/logout.php'>Logout</a><br></li>
+            ?>
+            <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
+            <li><img class='icon' src='../img/icons/logout.png' alt='Logout' ><a href='../controllers/logout.php'>Logout</a><br></li>
         </ul>
     </nav>
-    <div class="content">
-        <div class="page-header">            
-            <h2>Invoice's</h2>
-        </div>       
-        <form method="post" action="displayInvoice.php">
-            <table class="adminTable" id="adminTable">
-                <tr class="ui-widget-header ">
-                    <th>Total Amount</th>
-                    <th>Transaction ID</th>
-                    <th>Display Invoice</th>
-                </tr>
-                <?php
-                for ($i = 0; $i < count($invoice->invoiceTotal); $i++) {
-                    ?>
-                    <tr>
-                        <td >$<?php echo number_format($invoice->invoiceTotal[$i], 2) ?></td>
-                        <td><?php echo $invoice->invoiceTranId[$i] ?></td>
-                        <td><button class="invoiceButton" type="submit" name="invoiceId" value=<?php echo $invoice->invoiceTranId[$i] ?> id="submit">Display Invoice</button></td>                 
+    <div id="page">
+        <div class="content">
+            <div class="page-header">            
+                <h2>Invoice's</h2>
+            </div>       
+            <form method="post" action="displayInvoice.php">
+                <table class="adminTable" id="adminTable">
+                    <tr class="ui-widget-header ">
+                        <th>Total Amount</th>
+                        <th>Transaction ID</th>
+                        <th>Display Invoice</th>
                     </tr>
-                <?php } ?>
-            </table>
-        </form>
+                    <?php
+                    for ($i = 0; $i < count($invoice->invoiceTotal); $i++) {
+                        ?>
+                        <tr>
+                            <td >$<?php echo number_format($invoice->invoiceTotal[$i], 2) ?></td>
+                            <td><?php echo $invoice->invoiceTranId[$i] ?></td>
+                            <td><button class="invoiceButton" type="submit" name="invoiceId" value=<?php echo $invoice->invoiceTranId[$i] ?> id="submit">Display Invoice</button></td>                 
+                        </tr>
+                    <?php } ?>
+                </table>
+            </form>
+        </div>
     </div>
+    <div id="loading"></div>
 
 
