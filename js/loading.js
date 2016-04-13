ca1898d7fbc2b3744d088f34a98fb81c927fdd47
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-13 15:09:40 -0700

    Fixing the loading gif to fire on change of page.

--- js/loading.js
+++ js/loading.js
@@ -1,24 +1,19 @@
+function onClick(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
 
-    $('a').click(function () {
-
-        function onClick(callback) {
-            var intervalID = window.setInterval(checkReady, 1000);
-
-            function checkReady() {
-                if (document.getElementsByTagName('div')[0] !== undefined) {
-                    window.clearInterval(intervalID);
-                    callback.call(this);
-                }
-            }
+    function checkReady() {
+        if (document.getElementsByTagName('div')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
         }
+    }
+}
 
-        function show(id, value) {
-            document.getElementById(id).style.display = value ? 'block' : 'none';
-        }
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
 
-        onClick(function () {
-            show('page', false);
-            show('loading', true);
-        });
-        alert('ho ho ho');
-    });
+onClick(function () {
+    show('page', true);
+    show('loading', false);
+});
