efe864930ce7f5000b5f89a17f3ce38c36f3714a
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-13 09:55:47 -0700

    Fixed loading gif to work on click and stop on load. Tested and works

--- htm/portal/products.php
+++ htm/portal/products.php
@@ -14,7 +14,6 @@ require '../controllers/cart.db.php';
     <link href="../../css/styles.css" type="text/css" rel="stylesheet"/>
     <script src='../../js/ms-style-menu.js'></script>
     <script src='../../js/ms-style-cart.js'></script>
-    <script src="../../js/loading.js"></script>
     <script src='../../js/main.js'></script>
 </head>
 <div id="horizontalNav">
