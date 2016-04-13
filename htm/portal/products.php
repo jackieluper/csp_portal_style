2c82a94995c531e163d694e93f3c1aa9ee23db90
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-13 10:42:24 -0700

    New images added from Jackie

--- htm/portal/products.php
+++ htm/portal/products.php
@@ -25,7 +25,6 @@ require '../controllers/cart.db.php';
     </div>
 </div>
 <nav class="menu">
-    <a href="#" class="nav-toggle-btn">Menu</a>
     <ul>
         <?php if ($_SESSION['role'] >= $userRole) { ?>
             <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
