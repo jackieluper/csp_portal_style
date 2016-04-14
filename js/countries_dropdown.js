f3580ed095452554ee1d41d9303707c786d074d0
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-14 13:15:37 -0700

    Creating prepared sql statement for add-cust.php

--- js/countries_dropdown.js
+++ js/countries_dropdown.js
@@ -21,8 +21,7 @@ function print_country(country_id){
 function print_state(state_id, state_index){
 	var option_str = document.getElementById(state_id);
 	option_str.length=0;
-	option_str.options[0] = new Option('Select State','');
-	option_str.selectedIndex = 0;
+	
 	var state_arr = s_a[state_index].split("|");
 	for (var i=0; i<state_arr.length; i++) {
 		option_str.options[option_str.length] = new Option(state_arr[i],state_arr[i]);
