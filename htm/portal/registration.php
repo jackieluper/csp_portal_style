4893a0e267ec0fdfd2138fdfc8c113634302ddb3
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-14 13:25:13 -0700

    Fixing country state dropdowns.

--- htm/portal/registration.php
+++ htm/portal/registration.php
@@ -81,9 +81,9 @@ require '../controllers/config.php'
                 <th class="regTbl"></th>
             </tr>
             <tr >
-                <td class="row3"><select class="countrySelect" onchange="print_state('state', '1');" id="country" name ="country" required></select>
+                <td class="row3"><select class="countrySelect" onload="print_state('state', this.selectedIndex);" id="country" name ="country" required></select>
                     <script language="javascript">print_country("country");</script></td>
-                <td class="row3"><select class="countrySelect" name="state" id="state" onchange="(getState())" required></select></td>
+                <td class="row3"><select class="countrySelect" name="state" id="state" onload="(getState())" required></select></td>
                 <td class="row3" ><input class="form-control" type="text" name="zip" id="zip" placeholder="required" onkeyup="validateZip(this)" required></td>
                 <td class="row3">
                 </td>
