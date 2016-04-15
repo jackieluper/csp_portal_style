753083cec67c703ef17c15fc56545af166db2ec1
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-15 12:44:07 -0700

    Adding email function to update qty for invoice to be sent to user and to us.

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -71,7 +71,7 @@ if ($provision == 1) {
     $sqlInvoice = "INSERT INTO transactions(customer_id, item_num, sku, product_name, subscription_length, product_cost, qty, discount_rate, total_savings, total, transaction_id)
             VALUES('$customer_id', '1', '$subscription_id', '$subscription_name', '1 month(s)', '$erp_price', '$updateQty', '$discount', '$totalSavings', '$total', $tranId)";
     echo $customer_id;
-    if ($conn->query($sqlInvoice) == TRUE) {
+    if ($conn->query($sqlInvoice) == TRUE && $updateQty > 0) {
         $getEmailStmt = "SELECT email from user where customer_id='$customer_id'";
         $getEmailRes = $conn->query($getEmailStmt);
         if ($getEmailRes->num_rows > 0) {
