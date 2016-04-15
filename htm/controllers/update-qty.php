30634e8ade22d13fa2d5d90b4ab9b1cf895889af
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-15 12:39:56 -0700

    Adding email function to update qty for invoice to be sent to user and to us.

--- htm/controllers/update-qty.php
+++ htm/controllers/update-qty.php
@@ -79,32 +79,38 @@ if ($provision == 1) {
                 $email = $row['email'];
                 echo $email;
             }
+
             $_SESSION['invoiceId'] = $tranId;
+
             $subject = "Invoice #$tranId";
             $message = "<div ><img class='invoiceLogo' src='../img/MS_Logo_orange_small.png' alt=' $companyName '></div>
                     <div style='font-size: 24px;'><strong>Order ID: '$invoiceId' </strong></div><br>";
 
-            for ($i = 0; $i < count($invoiceReceipt->getSubscriptionId()); $i++) {
-                $cost = number_format($invoiceReceipt->productCost[$i], 2);
-                $qty = number_format($invoiceReceipt->productQty[$i], 0);
-                $invoiceReceipt = $invoiceReceipt->itemNum[$i];
-                $name = $invoiceReceipt->productName[$i];
-                $subscriptionId = $invoiceReceipt->subscriptionId[$i];
-                $message = $message + "$invoiceReceipt "
+            $invoiceReceipt = new invoiceReceipt();
+
+            $result = $conn->query("SELECT * FROM transactions where transaction_id='" . $invoiceId . "'");
+            while ($row = $result->fetch_assoc()) {
+                $subscriptionId = $row['sku'];
+                $itemNum = $row['item_num'];
+                $productName = $row['product_name'];
+                $subscriptionLength = $row['subscription_length'];
+                $productCost = $row['product_cost'];
+                $qty = $row['qty'];
+                $tranTotal = $row['total'];
+                $discount = $row['discount_rate'];
+                $totalSavings = $row['total_savings'];
+                $message = $message + "$itemNum "
                         . "--------------"
-                        . "Product Name: $name"
+                        . "Product Name: $productName"
                         . "Product ID: $subscriptionId"
-                        . "Subscription Length: 1 Month(s) "
-                        . "Product Cost: $ $cost "
+                        . "Subscription Length: $subscriptionLength "
+                        . "Product Cost: $ $productCost "
                         . "Product Quantity: $qty ";
             }
-            $rate = number_format($invoiceReceipt->discountRate, 2);
-            $savings = number_format($invoiceReceipt->totalSavings, 2);
-            $total = number_format($invoiceReceipt->invoiceTotal, 2);
             $message = $message +
-                    "Discount Rate:  $rate % "
-                    . "Total Savings: $savings "
-                    . "Sale Total: $ $total ";
+                    "Discount Rate:  $discount % "
+                    . "Total Savings: $totalSavings "
+                    . "Sale Total: $ $tranTotal ";
 
             $bcc = 'jsmith@managedsolution.com,jasonbsmith1568@yahoo.com';
             mail_utf8($email, $subject, $message, $bcc);
