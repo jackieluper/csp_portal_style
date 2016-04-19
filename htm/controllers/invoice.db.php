91308439e076109bb8718ed26127a8c966fed545
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-19 13:08:09 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/controllers/invoice.db.php
+++ htm/controllers/invoice.db.php
@@ -12,7 +12,7 @@ include '../classes/invoice.class.php';
 $invoice = new invoice();
 $index = 0;
 
-$getCustTran = "SELECT total, transaction_id FROM transactions where customer_id='" . $_SESSION['custId'] . "' GROUP BY transaction_id ORDER BY transaction_id";
+$getCustTran = "SELECT total, transaction_id FROM transactions where customer_ids='" . $_SESSION['custId'] . "' GROUP BY transaction_id ORDER BY transaction_id";
 $custTransRes = $conn->query($getCustTran);
 while ($row = $custTransRes->fetch_assoc()) {
     $tranTotal = $row['total'];
