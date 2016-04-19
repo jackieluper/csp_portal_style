6aee3de59c28b7e4ece434db0ffe0e13923c76e3
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-19 13:07:27 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/controllers/invoice.db.php
+++ htm/controllers/invoice.db.php
@@ -6,14 +6,14 @@ Managed Solution
 <?php
 session_start();
 include 'config.php';
+require "../api/client/_init.php";
 include '../classes/invoice.class.php';
 
 $invoice = new invoice();
 $index = 0;
-try{
-$getCustTran = "SELECT total, transaction_id FROM transactions where customer_ids='" . $_SESSION['custId'] . "' GROUP BY transaction_id ORDER BY transaction_id";
-$custTransRes = $conn->query($getCustTran);
 
+$getCustTran = "SELECT total, transaction_id FROM transactions where customer_id='" . $_SESSION['custId'] . "' GROUP BY transaction_id ORDER BY transaction_id";
+$custTransRes = $conn->query($getCustTran);
 while ($row = $custTransRes->fetch_assoc()) {
     $tranTotal = $row['total'];
     $tranId = $row['transaction_id'];
@@ -22,6 +22,3 @@ while ($row = $custTransRes->fetch_assoc()) {
     $index++;
 }
 
-}catch(Exception $e){
-     echo $e->errorMessage();
-}
