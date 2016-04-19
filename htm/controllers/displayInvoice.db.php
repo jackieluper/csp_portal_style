aae27ded1fa008ae75b311226694c2326c9c540b
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-19 12:48:51 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/controllers/displayInvoice.db.php
+++ htm/controllers/displayInvoice.db.php
@@ -19,29 +19,35 @@ $index = 0;
 
 $invoiceReceipt = new invoiceReceipt();
 
-$result = $conn->query("SELECT * FROM transactions where transaction_id='" . $invoiceId . "'");
-while ($row = $result->fetch_assoc()) {
-    $subscriptionId = $row['sku'];
-    $itemNum = $row['item_num'];
-    $productName = $row['product_name'];
-    $subscriptionLength = $row['subscription_length'];
-    $productCost = $row['product_cost'];
-    $qty = $row['qty'];
-    $tranTotal = $row['total'];
-    $discount = $row['discount_rate'];
-    $totalSavings = $row['total_savings'];
+try {
+    $getTranDetails = "SELECT * FROM transactions where transaction_id='$invoiceId'";
+    $tranDetailRes = $conn->query($getTranDetails);
 
-    $invoiceReceipt->setSubscriptionId($index, $subscriptionId);
-    $invoiceReceipt->setItemNum($index, $itemNum);
-    $invoiceReceipt->setProductName($index, $productName);
-    $invoiceReceipt->setSubscriptionLength($index, $subscriptionLength);
-    $invoiceReceipt->setProductCost($index, $productCost);
-    $invoiceReceipt->setProductQty($index, $qty);
-    $index++;
+    while ($row = $tranDetailRes->fetch_assoc()) {
+        $subscriptionId = $row['sku'];
+        $itemNum = $row['item_num'];
+        $productName = $row['product_name'];
+        $subscriptionLength = $row['subscription_length'];
+        $productCost = $row['product_cost'];
+        $qty = $row['qty'];
+        $tranTotal = $row['total'];
+        $discount = $row['discount_rate'];
+        $totalSavings = $row['total_savings'];
 
-    $invoiceReceipt->setInvoiceTotal($tranTotal);
-    $invoiceReceipt->setDiscountRate($discount);
-    $invoiceReceipt->setTotalSavings($totalSavings);
-}
+        $invoiceReceipt->setSubscriptionId($index, $subscriptionId);
+        $invoiceReceipt->setItemNum($index, $itemNum);
+        $invoiceReceipt->setProductName($index, $productName);
+        $invoiceReceipt->setSubscriptionLength($index, $subscriptionLength);
+        $invoiceReceipt->setProductCost($index, $productCost);
+        $invoiceReceipt->setProductQty($index, $qty);
+        $index++;
+
+        $invoiceReceipt->setInvoiceTotal($tranTotal);
+        $invoiceReceipt->setDiscountRate($discount);
+        $invoiceReceipt->setTotalSavings($totalSavings);
+    }
+} catch (Exception $e) {
+    echo $e->errorMessage();
+} 
 
 
