d309e9ac1abbd10e3dd56331f052995907312968
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-19 11:09:25 -0700

    Cleaning up code

--- htm/controllers/add-to-cart.php
+++ htm/controllers/add-to-cart.php
@@ -4,7 +4,9 @@ Date: 2/17/16
 Managed Solution
 -->
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require("config.php");
 require "../api/client/_init.php";
 
@@ -19,60 +21,55 @@ $subscriptionList = $subscription->getSubscriptionList();
 $sf_offer_id = $conn->real_escape_string($_POST['id']);
 $sf_user_name = $conn->real_escape_string($_SESSION['user']);
 $qty = intval($_POST['qty']);
+
 //getting last transaction id
 $sqlGetTran = "SELECT transaction_id FROM transactions ORDER BY transaction_id DESC LIMIT 1";
 $tranResult = $conn->query($sqlGetTran);
 if ($tranResult->num_rows > 0) {
     while ($row = $tranResult->fetch_assoc()) {
-        $transactionId = $row['transaction_id'] + 2;
+        $transactionId = $row['transaction_id'] + 1;
     }
 } else {
     $transactionId = 1;
 }
-//setting query to get customer id for the user that has logged on
-$sql = "SELECT customer_id from user WHERE username='$sf_user_name'";
-$result = $conn->query($sql);
 
+//setting query to get customer id for the user that has logged on
+$getCustId = "SELECT customer_id from user WHERE username='$sf_user_name'";
+$custIdRes = $conn->query($getCustId);
 //grabbing the customer id if exists and storing it to php variable
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+if ($custIdRes->num_rows > 0) {
+    while ($row = $custIdRes->fetch_assoc()) {
         $custID = $row['customer_id'];
     }
 }
+
 //Grabbing the sku for the product selected
-$sqlId = "SELECT sku, offer_uri from offer WHERE id='$sf_offer_id'";
-$resultId = $conn->query($sqlId);
-if ($resultId->num_rows > 0) {
-    while ($row = $resultId->fetch_assoc()) {
+$getProdSku = "SELECT sku, offer_uri from offer WHERE id='$sf_offer_id'";
+$prodSkuRes = $conn->query($getProdSku);
+if ($prodSkuRes->num_rows > 0) {
+    while ($row = $prodSkuRes->fetch_assoc()) {
         $offerSku = $row['sku'];
         $offerUri = $row['offer_uri'];
-        echo 'test2';
     }
 }
 
 //setting query to get our price, and resale price for the offer selected
-$sql1 = "SELECT list_price, erp_price from offer_price WHERE offer_id='$sf_offer_id'";
-$result1 = $conn->query($sql1);
-//grabbing our price and resale price(erp)
-if ($result1->num_rows > 0) {
-    while ($row = $result1->fetch_assoc()) {
+$getProdPrice = "SELECT list_price, erp_price from offer_price WHERE offer_id='$sf_offer_id'";
+$prodPriceRes = $conn->query($getProdPrice);
+if ($prodPriceRes->num_rows > 0) {
+    while ($row = $prodPriceRes->fetch_assoc()) {
         $erpPrice = $row['erp_price'];
         $listPrice = $row['list_price'];
-        echo 'test3';
     }
 }
-//setting query to get item already in cart and increment
-$sql3 = "SELECT qty from cart where items='$sf_offer_id'";
-$result3 = $conn->query($sql3);
 
 //setting query to get the display name from offers
-$sql5 = "SELECT display_name from offer where id='$sf_offer_id'";
-$result5 = $conn->query($sql5);
+$getProdName = "SELECT display_name from offer where id='$sf_offer_id'";
+$prodNameRes = $conn->query($getProdName);
 //grabbing the display name from offers
-if ($result5->num_rows > 0) {
-    while ($row = $result5->fetch_assoc()) {
+if ($prodNameRes->num_rows > 0) {
+    while ($row = $prodNameRes->fetch_assoc()) {
         $offerName = $row['display_name'];
-        echo 'test4';
     }
 }
 
@@ -91,15 +88,18 @@ for ($i = 0; $i < count($subscriptionList); $i++) {
 
 if ($found == false) {
 //query to add the selected item to the cart with corresponding customer info
-    $sql2 = "INSERT INTO cart (customer_id, items, item_name, our_cost, msrp, proposed_cost, qty, transaction_id, updat_qty, sku, offer_uri) 
-VALUES ('$custID', '$sf_offer_id', '$offerName', '$listPrice', '$erpPrice', '$erpPrice', '$qty', '$transactionId', '0', '$offerSku', '$offerUri')" or die(mysql_error());
+    $insCart = "INSERT INTO cart (customer_id, items, item_name, our_cost, msrp, proposed_cost, qty, transaction_id, updat_qty, sku, offer_uri) 
+             VALUES ('$custID', '$sf_offer_id', '$offerName', '$listPrice', '$erpPrice', '$erpPrice', '$qty', '$transactionId', '0', '$offerSku', '$offerUri')" or die(mysql_error());
 
+//setting query to get item already in cart and increment
+    $getQtyCart = "SELECT qty from cart where items='$sf_offer_id'";
+    $qtyCartRes = $conn->query($getQtyCart);
 //if they already have in cart increment qty 
-    if ($result3->num_rows > 0) {
-        while ($row = $result3->fetch_assoc()) {
+    if ($qtyCartRes->num_rows > 0) {
+        while ($row = $qtyCartRes->fetch_assoc()) {
             $qty = $row['qty'] + $qty;
-            $sql4 = "UPDATE cart SET qty='" . $qty . "' where items='" . $sf_offer_id . "'";
-            if ($conn->query($sql4) === TRUE) {
+            $updQtyCart = "UPDATE cart SET qty='" . $qty . "' where items='" . $sf_offer_id . "'";
+            if ($conn->query($updQtyCart) === TRUE) {
                 header('Location: ../portal/products.php');
             } else {
                 echo "Error updating record: " . $conn->error;
@@ -107,12 +107,10 @@ VALUES ('$custID', '$sf_offer_id', '$offerName', '$listPrice', '$erpPrice', '$er
         }
     }
 //else just add to the cart and redirect back to product page
-    else if ($conn->query($sql2) === TRUE) {
+    else if ($conn->query($insCart) === TRUE) {
         header('Location: ../portal/products.php');
     } else {
-        echo "Error: " . $sql2 . "<br>" . $conn->error;
+        echo "Error: " . $insCart . "<br>" . $conn->error;
     }
 }
-//close DB connection
-$conn->close();
 ?>
--- htm/controllers/logout.php
+++ htm/controllers/logout.php
@@ -5,18 +5,16 @@ Managed Solution
 -->
 <?php
 require("config.php");
-session_start();
+
 //Removes everything from cart on sign out
 $sql = "DELETE from cart";
 $result = $conn->query($sql);
-//If succesful do nothing
-if ($conn->query($sql) === TRUE) {
-    
-}
-//Errors out if does not remove cart items
-else {
+
+//If removing from cart fails through error
+if (!$conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 //Error reporting
 error_reporting(0);
 ob_start("ob_gzhandler");
