4c5b59875bcc08a9004478da5715263107540647
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-19 11:20:41 -0700

    Cleaning up code

--- htm/controllers/config.php
+++ htm/controllers/config.php
@@ -40,9 +40,5 @@ $password = "Ltd6530*%";
 $dbname = "discount_estimates";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
 
