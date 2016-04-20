c5c7ea4bf4c6af7f2cb9feee7b1e5adc2959bc31
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 09:39:00 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,19 @@
+function onClick(callback) {
+    var intervalID = window.setInterval(checkReady, 500);
 
+    function checkReady() {
+        if (document.getElementsByTagName('#icon')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
 
-$(document).ready()(function () {
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onClick(function () {
     show('page', true);
     show('loading', false);
-    $(document).click(function () {
-        show('page', false);
-        show('loading', true);
-
-    });
 });
