51ccff357e868968cda43df23cf6938efe098732
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 14:47:10 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/portal/displayInvoice.php
+++ htm/portal/displayInvoice.php
@@ -29,7 +29,7 @@ $invoiceId = $_POST['invoiceId'];
             </ul>
         </div>
     </div>
-    <nav class="menu">
+    <nav class="menu" ng-app="">
         <a href="#" class="nav-toggle-btn">Menu</a>
         <ul>
             <li ng-if="<?php $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
@@ -37,7 +37,7 @@ $invoiceId = $_POST['invoiceId'];
             <li ng-if="<?php $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
             <li ng-if="<?php $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
             <li ng-if="<?php $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
-            <li ng-hide="<?php $_SESSION['role'] != $adminRole ?>"><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
+            <li ng-show="<?php $_SESSION['role'] = $adminRole ?>"><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
             <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
             <li><img class='icon' src='../img/icons/logout.png' alt='Logout' ><a href='../controllers/logout.php'>Logout</a><br></li>
         </ul>
