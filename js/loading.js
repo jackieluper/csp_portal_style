7c625c9c683f1dcd8297a2e5303ec49c28633173
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 08:50:40 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,10 @@ $( window ).click(function(callback){
     var intervalID = window.setInterval(checkReady, 500);
 
     function checkReady() {
-        if (document.getElementsByTagName('div')[0] !== undefined) {
+        if ($(window).finish(function () {
+    show('page', true);
+    show('loading', false);
+})) {
             window.clearInterval(intervalID);
             callback.call(this);
         }
@@ -13,7 +16,3 @@ function show(id, value) {
     document.getElementById(id).style.display = value ? 'block' : 'none';
 }
 
-$(window).finish(function () {
-    show('page', true);
-    show('loading', false);
-});
