3a1d274250119e334699dfafdffaeab1a84fda25
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 14:51:13 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/portal/displayInvoice.php
+++ htm/portal/displayInvoice.php
@@ -4,10 +4,14 @@ Date: 3/21/16
 Managed Solution
 -->
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require("../controllers/config.php");
 include '../controllers/display-invoice.db.php';
 
 $invoiceId = $_POST['invoiceId'];
+
 ?>
 <head>
     <title>Display Invoice</title>
@@ -29,15 +33,21 @@ $invoiceId = $_POST['invoiceId'];
             </ul>
         </div>
     </div>
-    <nav class="menu" ng-app="">
+    <nav class="menu">
         <a href="#" class="nav-toggle-btn">Menu</a>
         <ul>
-            <li ng-if="<?php echo $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
-            <li ng-if="<?php echo $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/subscriptions.png' alt='Manage Subscription'><a href="../portal/manageSubscription.php">Manage Licenses</a><br></li>
-            <li ng-if="<?php echo $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
-            <li ng-if="<?php echo $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
-            <li ng-if="<?php echo $_SESSION['role'] >= $userRole ?>"><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
-            <li ng-show="<?php echo $_SESSION['role'] == $adminRole ?>"><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
+            <?php if ($_SESSION['role'] >= $userRole) { ?>
+                <li><img class='icon' src='../img/icons/software.png' alt='Products' ><a href='../portal/products.php'>Products</a><br></li>
+                <li><img class='icon' src='../img/icons/subscriptions.png' alt='Manage Subscription'><a href="../portal/manageSubscription.php">Manage Licenses</a><br></li>
+                <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
+                <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
+                <li><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
+                <?php if ($_SESSION['role'] == $adminRole) { ?>
+                    <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
+                    <?php
+                }
+            }
+            ?>
             <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
             <li><img class='icon' src='../img/icons/logout.png' alt='Logout' ><a href='../controllers/logout.php'>Logout</a><br></li>
         </ul>
