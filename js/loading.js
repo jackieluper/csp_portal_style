29906ae01451dd99d7da661f8f4c2c15b618c527
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 09:36:35 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,11 @@
-$(document).click(function () {
-    show('page', false);
-    show('loading', true);
 
-});
 
 $(document).ready()(function () {
     show('page', true);
     show('loading', false);
+    $(document).click(function () {
+        show('page', false);
+        show('loading', true);
+
+    });
 });
