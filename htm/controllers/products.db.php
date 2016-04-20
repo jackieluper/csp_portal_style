6364cbf3f38987902aa9886d74725ac912673d73
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 11:24:37 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/controllers/products.db.php
+++ htm/controllers/products.db.php
@@ -1,13 +1,56 @@
 <?php
+
 include 'config.php';
 include '../classes/offers.class.php';
 
 $index = 0;
 $entity = $_SESSION['entity'];
+$topOffers = new topOffers();
 $offers = new offers();
-echo $entity;
+
 try {
-    $getOfferDetails = "SELECT top_offer, offer.id, offer.display_name, offer.license_agreement_type, offer.purchase_unit, offer.sku, offer_price.erp_price FROM offer, offer_price WHERE offer.id=offer_id AND offer.license_agreement_type='$entity'";
+//Set the id depending on entity type
+    if ($entity === 'Corporate') {
+//SETS OFFER BASED ON OFFER ID
+        $hotOffers = array('55', '11', '112');
+    } else if ($entity === 'Government') {
+        $hotOffers = array('56', '12', '113');
+    }
+
+    for ($i = 0; $i < count($hotOffers); $i++) {
+        //Grabbing the product information to display in top offers
+        $getTopOfferDetails = "SELECT offer.id, offer.display_name, erp_price FROM offer, offer_price WHERE offer.id='$hotOffers[$i]' AND offer_id='$hotOffers[$i]'";
+        $topOfferDetailRes = $conn->query($getTopOfferDetails);
+        if ($topOfferDetailRes->num_rows > 0) {
+            while ($row = $topOfferDetailRes->fetch_assoc()) {
+                $name = $row['display_name'];
+                $price = $row['erp_price'];
+                $id = $row['id'];
+            }
+        } else {
+            throw new Exception("MySql Error: " . $getTopOfferDetails . "<br>" . $conn->error);
+        }
+        //creating topOffers->object TODO: MAKE JUST OFFER OBJECT AND REFERENCE TOPOFFERS IN DB
+        $topOffers->setOffer($index, $offer);
+        $topOffers->setOfferDetails($index, $name, $price);
+        $topOffers->setOfferId($index, $hotOffers[$i]);
+        $getImgSet = "SELECT img_tag, details FROM image WHERE offer_name='$name'";
+        $imgSetRes = $conn->query($getImgSet);
+        if ($imgSetRes->num_rows > 0) {
+            while ($row1 = $imgSetRes->fetch_assoc()) {
+                $tag = $row1['img_tag'];
+                $caption = $row1['details'];
+                $topOffers->setOfferImg($index, $tag);
+                $topOffers->setOfferCaption($index, $caption);
+            }
+        } else {
+            $tag = "noImage.png";
+            $topOffers->setOfferImg($index, $tag);
+            throw new Exception("MySql Error: " . $imgSetRes . "<br>" . $conn->error);
+        }
+        $index++;
+    }
+    $getOfferDetails = "SELECT offer.id, offer.display_name, offer.license_agreement_type, offer.purchase_unit, offer.sku, offer_price.erp_price FROM offer, offer_price WHERE offer.id=offer_id AND offer.license_agreement_type='$entity'";
     $offerDetailsRes = $conn->query($getOfferDetails);
     $index = 0;
     if ($offerDetailsRes->num_rows > 0) {
@@ -16,13 +59,11 @@ try {
             $erp = $row['erp_price'];
             $purchase_unit = $row['purchase_unit'];
             $id = $row['id'];
-            $topOffers = $row['top_offer'];
             $offers->setOfferName($index, $name);
             $offers->setOfferPrice($index, $erp);
             $offers->setOfferUnit($index, $purchase_unit);
             $offers->setOfferId($index, $id);
-            $offers->setTopOffer($index, $topOffer);
-
+            
             $getImgSet = "SELECT img_tag, details FROM image WHERE offer_name='$name'";
             $imgSetRes = $conn->query($getImgSet);
             if ($imgSetRes->num_rows > 0) {
