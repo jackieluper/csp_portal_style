b7617c0190a7fbb44956cfe6d0d9c81236c12b42
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 14:32:20 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/portal/displayInvoice.php
+++ htm/portal/displayInvoice.php
@@ -39,10 +39,8 @@ $invoiceId = $_POST['invoiceId'];
                 <li><img class='icon' src='../img/icons/invoice.jpg' alt='Invoice'><a href='../portal/invoice.php'>Invoice</a><br></li>
                 <li><img class='icon' src='../img/icons/checkout.png' alt='Checkout' ><a href='../portal/checkout.php'>Checkout</a><br></li>
                 <li><img class='icon' src='../img/icons/home.png' alt='Home' ><a href='<?php echo $homePage ?>'><?php echo $companyName ?></a><br></li>
-                <?php if ($_SESSION['role'] == $adminRole) { ?>
-                    <li><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
+                <li ng-if="<?php $_SESSION['role'] == $adminRole ?>"><img class='icon' src='../img/icons/Admin.png' alt='Admin' ><a href='../portal/admin.php'>Administration</a><br></li>
                     <?php
-                }
             }
             ?>
             <li><img class='icon' src='../img/icons/contact.jpg' alt='Contact' ><a href='../portal/contactUs.php'>Contact us</a><br></li>
