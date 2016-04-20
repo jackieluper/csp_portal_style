e3689fb84f3986cd09ac6156749f9db855a1bb3c
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-20 11:17:01 -0700

    Cleaning up code, to function better, cleaner, easier to read, easier to manage.

--- htm/classes/offers.class.php
+++ htm/classes/offers.class.php
@@ -5,87 +5,68 @@ Managed Solution
 -->
 <?php
 
-class topOffers {
-
-    function setOffer($index, $offer) {
-        $this->offer[$index] = $offer;
-    }
-    function getOffer() {
-        return $this->offer;
-    }
-    function setOfferDetails($index, $name, $price) {
-        $this->name[$index] = $name;
-        $this->price[$index] = $price;
-    }
-
-    function getOfferDetails() {
-        return $this->name . '<br>'
-                . '$' . number_format($this->price, 2);
-    }
-    function setOfferId($index, $id){
-        $this->id[$index] = $id;
-    }
-    function getOfferId(){
-        $this->id;
-    }
-     function setOfferImg($index, $tag){
-        $this->img_tag[$index] = "../img/microsoft_img/$tag";
-    }
-    function getOfferImg(){
-        $this->img_tag;
-    }
-    function setOfferCaption($index, $caption){
-        $this->caption[$index] = $caption;
-    }
-    function getOfferCaption(){
-        $this->caption;
-    }
-}
-
 class offers {
 
+    function setTopOffer($index, $topOffer) {
+        $this->topOffer[$index] = $topOffer;
+    }
+    function getTopOffer(){
+        $this->topOffers;
+    }
     function setOffer($offer) {
         $this->offer[0] = $offer;
     }
+
     function getOffer() {
         return $this->offer[0];
     }
-    function setOfferName($index, $name){
+
+    function setOfferName($index, $name) {
         $this->name[$index] = $name;
     }
-    function getOfferName(){
+
+    function getOfferName() {
         $this->name;
     }
-    function setOfferPrice($index, $price){
+
+    function setOfferPrice($index, $price) {
         $this->price[$index] = $price;
     }
-    function getOfferPrice(){
+
+    function getOfferPrice() {
         $this->price;
     }
-    function setOfferUnit($index, $unit){
+
+    function setOfferUnit($index, $unit) {
         $this->unit[$index] = $unit;
     }
-    function getOfferUnit(){
+
+    function getOfferUnit() {
         $this->unit;
     }
-    function setOfferId($index, $id){
+
+    function setOfferId($index, $id) {
         $this->id[$index] = $id;
     }
-    function getOfferId(){
+
+    function getOfferId() {
         $this->id;
     }
-    function setOfferImg($index, $tag){
+
+    function setOfferImg($index, $tag) {
         $this->img_tag[$index] = "../img/microsoft_img/$tag";
     }
-    function getOfferImg(){
+
+    function getOfferImg() {
         $this->img_tag;
     }
-    function setOfferCaption($index, $caption){
+
+    function setOfferCaption($index, $caption) {
         $this->caption[$index] = $caption;
     }
-    function getOfferCaption(){
+
+    function getOfferCaption() {
         $this->caption;
     }
-}
 
-    
+}
