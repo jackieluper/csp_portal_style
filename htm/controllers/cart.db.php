d82e6b8124710760919f58c67c3450f68b31a35f
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-25 08:25:47 -0700

    fixing bugs from error emails

--- htm/controllers/cart.db.php
+++ htm/controllers/cart.db.php
@@ -4,15 +4,22 @@ Date: 3/21/16
 Managed Solution
 -->
 <?php
+require '../classes/cart.class.php';
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-include '../classes/cart.class.php';
+if(isset($_SESSION['company_name'])){
+    $companyName = $_SESSION['company_name'];
+    $custId = $_SESSION['custId'];
+}
+else{
+    $companyName = null;
+}
 $index = 0;
 $cart = new cart();
 
 //Grabbing all the cart items to show on checkout page for the customer to make any last minute changes
-$result = $conn->query("select items, item_name, msrp, qty from cart");
+$result = $conn->query("select items, item_name, msrp, qty from cart where customer_id =$custId");
 
 $total = 0;
 while ($row = $result->fetch_assoc()) {
@@ -29,7 +36,7 @@ while ($row = $result->fetch_assoc()) {
 }
 $cart->setTotal($total);
 //Calculate customer discount if any and display savings as well as new total
-$sqlDiscount = "Select discount from customer where customer_name='" . $_SESSION['company_name'] . "'";
+$sqlDiscount = "Select discount from customer where customer_name='$companyName'";
 $resultDiscount = $conn->query($sqlDiscount);
 while ($row = $resultDiscount->fetch_assoc()) {
     $rate = $row['discount'];
