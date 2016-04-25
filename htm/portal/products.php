0b951e315bc706bdaf3c9bb56ae3de09a522c3d0
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-25 12:22:06 -0700

    Fixing img

--- htm/portal/products.php
+++ htm/portal/products.php
@@ -5,9 +5,9 @@ require '../controllers/cart.db.php';
 ?>
 <head>
     <title>Products</title>
-    <meta charset="utf-8">
+    <meta charset="utf-8"/>
     <meta http-equiv="refresh" content="180;url=http://www.msolcsptest.com/htm/controllers/logout.php" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
