8ce8e1d779e9ddee8f40a200b444b11b1eb22bb0
jsmith <jsmith@Dev3-E6530.MS.local>
2016-04-25 08:50:33 -0700

    Fixing bug: Company name not defined after session expired

--- htm/controllers/products.db.php
+++ htm/controllers/products.db.php
@@ -1,16 +1,15 @@
 <?php
 require 'config.php';
 require '../classes/offers.class.php';
-//Checking if session is still active
+
 if (session_status() == PHP_SESSION_NONE) {
-    header("Location: logout.php");
+    
 }
-//setting initial variables/objects
+$index = 0;
 $entity = $_SESSION['entity'];
 $offers = new offers();
 
 try {
-    //query to get offer details and to determine if it is a top offer or not
     $getOfferDetails = "SELECT top_offer, offer.id, offer.display_name, offer.license_agreement_type, offer.purchase_unit, offer.secondary_license_type, offer.sku, offer_price.erp_price FROM offer, offer_price WHERE offer.id=offer_id AND offer.license_agreement_type='$entity'";
     $offerDetailsRes = $conn->query($getOfferDetails);
     $index = 0;
@@ -23,32 +22,24 @@ try {
                 $purchase_unit = $row['purchase_unit'];
                 $id = $row['id'];
                 $topOffer = $row['top_offer'];
-                //setting DB data to offers object
                 $offers->setOfferName($index, $name);
                 $offers->setOfferPrice($index, $erp);
                 $offers->setOfferUnit($index, $purchase_unit);
                 $offers->setOfferId($index, $id);
                 $offers->setTopOffer($index, $topOffer);
-                //setting the img to the name of the corresponding offer
+
                 $getImgSet = "SELECT img_tag, details FROM image WHERE offer_name='$name'";
                 $imgSetRes = $conn->query($getImgSet);
                 if ($imgSetRes->num_rows > 0) {
                     while ($row1 = $imgSetRes->fetch_assoc()) {
                         $tag = $row1['img_tag'];
                         $caption = $row1['details'];
-
                         $offers->setOfferImg($index, $tag);
                         $offers->setOfferCaption($index, $caption);
                     }
-                    //if not
                 } else {
                     $tag = "noImage.png";
                     $offers->setOfferImg($index, $tag);
-                    $message = "No Image Exception: $getImgSet \n" ;
-                            " Product Name: $name";
-                    $email = 'jsmith@managedsolution.com';
-                    $bcc = "csperrors@managedsolution.com";
-                    mail_utf8($email, $subject, $message, $bcc);
                     throw new Exception("MySql Error: " . $getImgSet . "<br>" . $conn->error);
                 }
                 $index++;
