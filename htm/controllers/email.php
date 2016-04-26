b7e9e33d46b102f699a64f9d8711fb28718bee2d
Jason <Jason@Jason>
2016-04-26 08:53:27 -0700

    Fixed table width for older browsers

--- htm/controllers/email.php
+++ htm/controllers/email.php
@@ -3,7 +3,7 @@
 function mail_utf8($email, $subject, $message, $bcc) { 
       $from_user = "=?UTF-8?B?".base64_encode('Managed Solution')."?=";
       $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
-      $from_email = 'billing@managedsolution.com';
+      $from_email = 'cspbilling@managedsolution.com';
       $headers = "From: $from_user <$from_email>\r\n". 
                "MIME-Version: 1.0" . "\r\n" . 
                "Content-type: text/html; charset=UTF-8" . "\r\n" .
